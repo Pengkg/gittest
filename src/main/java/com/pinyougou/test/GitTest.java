cd2d9fc5e6b16652d8cd317bbc69e0de97445112
Pengkg <1158613432@qq.com>
2019-06-25 11:10:11 +0800

    第一次

---
+++ src/main/java/com/pinyougou/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.pinyougou.test;
+
+/**
+ * @BelongsProject: git_test
+ * @BelongsPackage: com.pinyougou.test
+ * @Author: pky
+ * @CreateTime: 2019-06-25 11:05
+ * @Description: ${Description}
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hah1");
+    }
+}
